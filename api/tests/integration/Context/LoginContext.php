a5ed428c95e94775e909a7d082941dea236da884
Anthonius Munthi <me@itstoni.com>
2021-08-20 15:34:11 +0800

    fixing e2e tests

--- api/tests/integration/Context/LoginContext.php
+++ api/tests/integration/Context/LoginContext.php
@@ -16,6 +16,7 @@ namespace Integration\EOffice\Context;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\MinkExtension\Context\MinkContext;
+use FriendsOfBehat\SymfonyExtension\Context\Environment\InitializedSymfonyExtensionEnvironment;
 
 class LoginContext implements Context
 {
@@ -28,9 +29,13 @@ class LoginContext implements Context
      * @param BeforeScenarioScope $scope
      * @BeforeScenario
      */
-    public function gatherContexts(BeforeScenarioScope $scope)
+    public function gatherContexts(BeforeScenarioScope $scope): void
     {
-        $this->minkContext = $scope->getEnvironment()->getContext(MinkContext::class);
+        /** @var InitializedSymfonyExtensionEnvironment $env */
+        $env = $scope->getEnvironment();
+        /** @var MinkContext $minkContext */
+        $minkContext       = $env->getContext(MinkContext::class);
+        $this->minkContext = $minkContext;
     }
 
     /**
