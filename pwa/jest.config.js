fc3724fe2c1c94ec227f67a3714f1a714142b746
Anthonius Munthi <me@itstoni.com>
2021-08-20 11:02:02 +0800

    configured coverage for pwa

--- pwa/jest.config.js
+++ pwa/jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   testMatch: [
     "**/src/**/*.spec.(js|jsx|ts|tsx)"
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+      "**/src/**/*.(js|jsx|ts|vue)",
+      "!**/src/**/*.spec.js"
   ]
 }
